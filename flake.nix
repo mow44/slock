1ec441082b2dd2bc3e9c12e1977753fff43755a8
mow44 <mow44@proton.me>
2025-06-28 14:33:49 +0300

    Add flake-utils

--- flake.nix
+++ flake.nix
@@ -1,46 +1,57 @@
 {
   description = "slock";
 
-  inputs.nixpkgs.url = "nixpkgs/nixpkgs-unstable";
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
 
   outputs =
-    { self, nixpkgs }:
-    let
-      system = "x86_64-linux";
-      pkgs = import nixpkgs { inherit system; };
-      configFile = import ./config.nix {
-        inherit pkgs;
-      };
-    in
     {
-      defaultPackage.${system} =
-        with pkgs;
-        stdenv.mkDerivation {
-          name = "slock";
-          version = "1.5";
-
-          src = self;
-
-          buildInputs = with pkgs; [
-            xorg.xorgproto
-            xorg.libX11
-            xorg.libXext
-            xorg.libXrandr
-            libxcrypt
-          ];
-
-          makeFlags = [ "CC:=$(CC)" ];
-          installFlags = [ "PREFIX=$(out)" ];
-
-          prePatch = ''
-            cp ${configFile} config.def.h
-          '';
-
-          postPatch = "sed -i '/chmod u+s/d' Makefile";
-
-          meta = {
-            mainProgram = "slock";
-          };
+      self,
+      nixpkgs,
+      flake-utils,
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+        configFile = import ./config.nix {
+          inherit pkgs;
         };
-    };
+      in
+      {
+        packages = rec {
+          slock = pkgs.stdenv.mkDerivation {
+            name = "slock";
+            version = "1.5";
+
+            src = self;
+
+            buildInputs = with pkgs; [
+              xorg.xorgproto
+              xorg.libX11
+              xorg.libXext
+              xorg.libXrandr
+              libxcrypt
+            ];
+
+            makeFlags = [ "CC:=$(CC)" ];
+            installFlags = [ "PREFIX=$(out)" ];
+
+            prePatch = ''
+              cp ${configFile} config.def.h
+            '';
+
+            postPatch = "sed -i '/chmod u+s/d' Makefile";
+
+            meta = {
+              mainProgram = "slock";
+            };
+          };
+
+          default = slock;
+        };
+      }
+    );
 }
